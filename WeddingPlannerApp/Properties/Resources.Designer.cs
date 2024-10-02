a0cd3388bb983771b9abc1b39bd7fa59f57efc24
LIYABONA NOLUBABALO DIAMOND <224097353@student.uj.ac.za>
2024-10-02 14:54:18 +0200

    Form 2

--- WeddingPlannerApp/Properties/Resources.Designer.cs
+++ WeddingPlannerApp/Properties/Resources.Designer.cs
@@ -63,9 +63,183 @@ namespace WeddingPlannerApp.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap _10_Best_Spring_Wedding_Color_Palette_Ideas_To_Help_Inspire_You_For_Your_Special_Day___Francisca_s_Bridal {
+            get {
+                object obj = ResourceManager.GetObject("10 Best Spring Wedding Color Palette Ideas To Help Inspire You For Your Special D" +
+                        "ay - Francisca\'s Bridal", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap _15__Brides_Who_Dared_to_Choose_a_Very_Unusual_Outfit_for_Their_Wedding_and_Stunned_Everyone {
+            get {
+                object obj = ResourceManager.GetObject("15+ Brides Who Dared to Choose a Very Unusual Outfit for Their Wedding and Stunne" +
+                        "d Everyone", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap _1pair_Gold_Leaf_Lapel_Pin_For_Men___Women_s_Shirt___Suit__Unisex_Lover_Gift_Collar_Clips {
+            get {
+                object obj = ResourceManager.GetObject("1pair Gold Leaf Lapel Pin For Men & Women\'s Shirt & Suit, Unisex Lover Gift Colla" +
+                        "r Clips", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap _22_Amazing_Details_for_A_Cosy_Winter_Wedding___weddingsonline {
+            get {
+                object obj = ResourceManager.GetObject("22 Amazing Details for A Cosy Winter Wedding _ weddingsonline", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap _34_Unique_Wedding_Food_Dessert_Table_Display_Ideas___Mrs_to_Be {
+            get {
+                object obj = ResourceManager.GetObject("34 Unique Wedding Food Dessert Table Display Ideas - Mrs to Be", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap _9_Enchanting_Spring_Wedding_Themes__Inspiring_Color_Ideas__Decor__Favors__and_More {
+            get {
+                object obj = ResourceManager.GetObject("9 Enchanting Spring Wedding Themes_ Inspiring Color Ideas, Decor, Favors, and Mor" +
+                        "e", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap A_Romantic_Pink_Miramonte_Resort_Wedding_in_Palm_Desert {
+            get {
+                object obj = ResourceManager.GetObject("A Romantic Pink Miramonte Resort Wedding in Palm Desert", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Afro_fusion_restaurant___Tabys_Café___Restaurant {
+            get {
+                object obj = ResourceManager.GetObject("Afro-fusion restaurant _ Tabys Café & Restaurant", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Agatha___Ivory___10___Extra_Length {
+            get {
+                object obj = ResourceManager.GetObject("Agatha - Ivory _ 10 _ Extra-Length", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap alphabet_word_images_1295152_1920 {
             get {
                 object obj = ResourceManager.GetObject("alphabet-word-images-1295152_1920", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Best_Butter_Chicken_Recipe {
+            get {
+                object obj = ResourceManager.GetObject("Best Butter Chicken Recipe", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Best_Cupcake_Ideas___Kitchen_Fun_With_My_3_Sons {
+            get {
+                object obj = ResourceManager.GetObject("Best Cupcake Ideas _ Kitchen Fun With My 3 Sons", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Black_tuxedo {
+            get {
+                object obj = ResourceManager.GetObject("Black tuxedo", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Bridal_rings_at_KAY {
+            get {
+                object obj = ResourceManager.GetObject("Bridal rings at KAY", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap download {
+            get {
+                object obj = ResourceManager.GetObject("download", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap download__1_ {
+            get {
+                object obj = ResourceManager.GetObject("download (1)", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap download__2_ {
+            get {
+                object obj = ResourceManager.GetObject("download (2)", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Drinks {
+            get {
+                object obj = ResourceManager.GetObject("Drinks", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
@@ -83,9 +257,81 @@ namespace WeddingPlannerApp.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Light_Grey_Three_Piece_Suit_for_Men___Formal_Wedding__Business__or_Special_Occasions___Tailored_Suit___The_Rising_Sun_store__Vardo {
+            get {
+                object obj = ResourceManager.GetObject("Light Grey Three Piece Suit for Men - Formal Wedding, Business, or Special Occasi" +
+                        "ons - Tailored Suit - The Rising Sun store, Vardo", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Nike__I_Do__Shoes {
+            get {
+                object obj = ResourceManager.GetObject("Nike \'I Do\' Shoes", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Pink_Dreams__16_Stylish_Ideas_for_a_Pink_Wedding {
+            get {
+                object obj = ResourceManager.GetObject("Pink Dreams_ 16 Stylish Ideas for a Pink Wedding", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap silhouette_3754208_1920 {
             get {
                 object obj = ResourceManager.GetObject("silhouette-3754208_1920", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Simon_Slim_Fit_High_Quality_Shawl_Collar_Black_Tuxedo_Suıt___EU_50___US_40__UK_40___36W {
+            get {
+                object obj = ResourceManager.GetObject("Simon Slim Fit High Quality Shawl Collar Black Tuxedo Suıt - EU 50 - US 40_ UK 40" +
+                        " _ 36W", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap snacks {
+            get {
+                object obj = ResourceManager.GetObject("snacks", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Stay_Golden___WedLuxe_Magazine {
+            get {
+                object obj = ResourceManager.GetObject("Stay Golden - WedLuxe Magazine", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap The_Best_Wedding_Drink_Menu_Ideas {
+            get {
+                object obj = ResourceManager.GetObject("The Best Wedding Drink Menu Ideas", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
@@ -106,6 +352,46 @@ namespace WeddingPlannerApp.Properties {
         internal static System.Drawing.Bitmap user_2935373_1920 {
             get {
                 object obj = ResourceManager.GetObject("user-2935373_1920", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Wedding___Bridal___Formal_Necklace_Set___Color__Silver___Size__Os {
+            get {
+                object obj = ResourceManager.GetObject("Wedding _ Bridal _ Formal Necklace Set _ Color_ Silver _ Size_ Os", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Wedding_Cake_Alternatives_Guide_for_2024___Wedding_Forward {
+            get {
+                object obj = ResourceManager.GetObject("Wedding Cake Alternatives Guide for 2024 _ Wedding Forward", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Wedding_drinks {
+            get {
+                object obj = ResourceManager.GetObject("Wedding drinks", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Wedding_gown {
+            get {
+                object obj = ResourceManager.GetObject("Wedding gown", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
